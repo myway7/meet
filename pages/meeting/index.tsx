56d7de141bc1006456f85cd3d76693a2c2fb8642
“myway7” <wangm847@gmail.com>
2023-10-01 11:29:27 +0800

    update

--- pages/meeting/index.tsx
+++ pages/meeting/index.tsx
@@ -79,10 +79,10 @@ const Home: NextPage = () => {
                 username: '',
                 videoEnabled: true,
                 audioEnabled: true,
-                // e2ee: !!e2eePassphrase,
-                // sharedPassphrase: e2eePassphrase || randomString(64),
-                e2ee: true,
+                e2ee: !!e2eePassphrase,
                 sharedPassphrase: e2eePassphrase || randomString(64),
+                // e2ee: true,
+                // sharedPassphrase: e2eePassphrase || randomString(64),
               }}
               onSubmit={handlePreJoinSubmit}
               showE2EEOptions={false}
